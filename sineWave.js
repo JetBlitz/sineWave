576889ac4126bf0bbf81d8a2dbf37608cdf33e7b
Eivin Adlawan <eadlawa2@gmail.com>
2020-03-27 13:42:05 -0500

    Savepoint: line moving left every second

--- sineWave.js
+++ sineWave.js
@@ -3,11 +3,12 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let start = 0;
+let xCoordinate = 1000
 // ctx.lineTo(1) //accepts x,y coordinates
 // ctx.moveTo // starting point
-// ctx.stroke
-
-ctx.moveTo()
+// ctx.stroke()
+ctx.beginPath();
+ctx.moveTo(1000, 500);
 
 const timer = setInterval(tick, 1000, 10);
 
@@ -16,7 +17,10 @@ function tick(end) {
     window.clearTimeout(timer)
     return
   } else {
+    ctx.lineTo(xCoordinate, 500)
+    ctx.stroke();
     start++
+    xCoordinate -= 100;
   }
   console.log(start);
   // console.log(end);
